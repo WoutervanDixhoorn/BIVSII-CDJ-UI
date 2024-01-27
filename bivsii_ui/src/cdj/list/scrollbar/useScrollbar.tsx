898dfe7f79ce37684a4e5d52a9db7a575b2413e6
Wouter van Dixhoorn <woutervdixhoorn@gmail.com>
2024-01-27 23:42:02 +0100

    Refactor: Fixed more indent error's

--- bivsii_ui/src/cdj/list/scrollbar/useScrollbar.tsx
+++ bivsii_ui/src/cdj/list/scrollbar/useScrollbar.tsx
@@ -22,64 +22,62 @@ export default function useScrollbar({scrollRef, handleRef}: scrollbarProps) {
   };
     
   const handleMouseMove = (event: any) => {
-      if (scrollContext.isDragging && scrollRef.current && handleRef.current) {
-        const deltaY = event.clientY - scrollContext.dragStartPos;
-        const barHeight = scrollRef.current.clientHeight;
-        const maxScroll = scrollContext.totalListCount - 6;
-        const scrollChange = (deltaY / barHeight) * maxScroll * scrollContext.scrollSens;
-        scrollContext.setCurScroll((prev) => clamp(prev + scrollChange, 0, maxScroll * scrollContext.scrollSens));
-        scrollContext.setDragStartPos(event.clientY);
-      }
+    if (scrollContext.isDragging && scrollRef.current && handleRef.current) {
+      const deltaY = event.clientY - scrollContext.dragStartPos;
+      const barHeight = scrollRef.current.clientHeight;
+      const maxScroll = scrollContext.totalListCount - 6;
+      const scrollChange = (deltaY / barHeight) * maxScroll * scrollContext.scrollSens;
+      scrollContext.setCurScroll((prev) => clamp(prev + scrollChange, 0, maxScroll * scrollContext.scrollSens));
+      scrollContext.setDragStartPos(event.clientY);
+    }
   };
 
   const handleScrollClick = (event: React.MouseEvent) => {
-      if (scrollRef.current) {
-          const clickPos = event.clientY - scrollRef.current.getBoundingClientRect().top;
-          const barHeight = scrollRef.current.clientHeight;
-          const maxScroll = scrollContext.totalListCount - 6;
-      
-          const scrollChange = (clickPos / barHeight) * maxScroll * scrollContext.scrollSens;
+    if (scrollRef.current) {
+      const clickPos = event.clientY - scrollRef.current.getBoundingClientRect().top;
+      const barHeight = scrollRef.current.clientHeight;
+      const maxScroll = scrollContext.totalListCount - 6;
+  
+      const scrollChange = (clickPos / barHeight) * maxScroll * scrollContext.scrollSens;
 
-          scrollContext.setCurScroll(clamp(scrollChange, 0, maxScroll * scrollContext.scrollSens));
-      }
-      event.preventDefault();
+      scrollContext.setCurScroll(clamp(scrollChange, 0, maxScroll * scrollContext.scrollSens));
+    }
+    event.preventDefault();
   };
 
   useEffect(() => {
-      const cleanup = () => {
-          window.removeEventListener("pointermove", handleMouseMove);
-          window.removeEventListener("pointerup", handleMouseUp);
-      };
+    const cleanup = () => {
+      window.removeEventListener("pointermove", handleMouseMove);
+      window.removeEventListener("pointerup", handleMouseUp);
+    };
       
-      if (scrollContext.isDragging) {
-          window.addEventListener("pointermove", handleMouseMove);
-          window.addEventListener("pointerup", handleMouseUp);
-          return cleanup;
-      }
+    if (scrollContext.isDragging) {
+      window.addEventListener("pointermove", handleMouseMove);
+      window.addEventListener("pointerup", handleMouseUp);
+      return cleanup;
+    }
   }, [scrollContext.isDragging, handleMouseUp]);
 
   useEffect(() => {
-      const curScrollPos = scrollContext.curScroll / scrollContext.scrollSens;
+    const curScrollPos = scrollContext.curScroll / scrollContext.scrollSens;
 
-      if (scrollRef.current && handleRef.current){
-          const barHeight = scrollRef.current.clientHeight;
+    if (scrollRef.current && handleRef.current){
+      const barHeight = scrollRef.current.clientHeight;
 
-          //Calc handle height
-          const contentHeight = ((barHeight/6)*scrollContext.totalListCount);
-          const visibleContentRatio = barHeight / contentHeight;
-          scrollContext.setHandleHeight(visibleContentRatio * barHeight);
+      //Calc handle height
+      const contentHeight = ((barHeight/6)*scrollContext.totalListCount);
+      const visibleContentRatio = barHeight / contentHeight;
+      scrollContext.setHandleHeight(visibleContentRatio * barHeight);
 
-          //Calc handle pos
-          const maxScroll = scrollContext.totalListCount - 6;
-          const normScroll = curScrollPos !== 0 ? curScrollPos / maxScroll : 0;
-          const hPos = (barHeight - scrollContext.handleHeight) * normScroll;
+      //Calc handle pos
+      const maxScroll = scrollContext.totalListCount - 6;
+      const normScroll = curScrollPos !== 0 ? curScrollPos / maxScroll : 0;
+      const hPos = (barHeight - scrollContext.handleHeight) * normScroll;
 
-          scrollContext.setHandlePos(hPos);
-
-          console.log(scrollContext.curScroll/ scrollContext.scrollSens);
-          
-          scrollContext.updateList();
-      }
+      //Update changes
+      scrollContext.setHandlePos(hPos);
+      scrollContext.updateList();
+    }
   }, [scrollContext.curScroll]);
 
   const handleStyle: React.CSSProperties = {
